b87b1e636afbee1002368f0c56912f0b975a3701
Charlie Stanard <louis.stanard@gmail.com>
2020-11-04 14:40:18 -0700

    Starting refactor without recursion, getting closer

--- src/utils/__tests__/getPlateCombinations.ts
+++ src/utils/__tests__/getPlateCombinations.ts
@@ -13,7 +13,9 @@ describe("getPlateCombinations", () => {
   describe("should return all possible combinations", () => {
     it("should return correct results for 2 plates", () => {
       const input = [45, 25];
-      expect(getPlateCombinations(input)).toEqual([[45], [25, 45], [25]]);
+      const result = getPlateCombinations(input);
+      expect(result.length).toEqual(3);
+      expect(result).toEqual([[45, 25], [45], [25]]);
     });
 
     it("should return correct results for 3 plates", () => {
@@ -21,13 +23,13 @@ describe("getPlateCombinations", () => {
       const result = getPlateCombinations(input);
       expect(result.length).toEqual(7);
       expect(result).toEqual([
+        [45, 25, 10],
         [45],
-        [10, 25, 45],
-        [25],
-        [10, 25],
+        [25, 10],
         [10],
-        [10, 45],
-        [25, 45],
+        [25],
+        [45, 10],
+        [45, 25],
       ]);
     });
 
@@ -36,21 +38,21 @@ describe("getPlateCombinations", () => {
       const result = getPlateCombinations(input);
       expect(result.length).toEqual(15);
       expect(result).toEqual([
+        [45, 25, 10, 5],
         [45],
-        [5, 10, 25, 45],
+        [25, 10, 5],
+        [10, 5],
+        [25, 5],
+        [25, 10],
         [25],
-        [5, 10, 25],
+        [45, 10, 5],
+        [45, 5],
+        [45, 10],
         [10],
-        [5, 10],
+        [45, 25, 5],
+        [45, 25],
         [5],
-        [5, 25],
-        [10, 25],
-        [5, 10, 45],
-        [5, 45],
-        [10, 45],
-        [5, 25, 45],
-        [25, 45],
-        [10, 25, 45],
+        [45, 25, 10],
       ]);
     });
 
@@ -59,29 +61,81 @@ describe("getPlateCombinations", () => {
       const result = getPlateCombinations(input);
       expect(result.length).toEqual(11);
       expect(result).toEqual([
+        [45, 25, 5, 5],
         [45],
-        [5, 5, 25, 45],
-        [25],
-        [5, 5, 25],
-        [5],
+        [25, 5, 5],
         [5, 5],
-        [5, 25],
-        [5, 5, 45],
-        [5, 45],
-        [5, 25, 45],
-        [25, 45],
+        [25, 5],
+        [25],
+        [45, 5, 5],
+        [45, 5],
+        [5],
+        [45, 25, 5],
+        [45, 25],
       ]);
     });
 
-    it("should return correct number of results for 5 plates", () => {
+    /**
+     * Current benchmarks to beat
+     */
+    // ✓ should return correct number of results for 5 plates (3ms)
+    // ✓ should return correct number of results for 6 plates (20ms)
+    // ✓ should return correct number of results for 7 plates (281ms)
+
+    // ✓ should return correct number of results for 5 plates (3ms)
+    // ✓ should return correct number of results for 6 plates (22ms)
+    // ✓ should return correct number of results for 7 plates (318ms)
+
+    // ✓ should return correct number of results for 5 plates (2ms)
+    // ✓ should return correct number of results for 6 plates (20ms)
+    // ✓ should return correct number of results for 7 plates (290ms)
+    // ✓ should return correct number of results for 8 plates (3888ms)
+
+    it.only("should return correct number of results for 5 plates", () => {
       const input = [45, 25, 10, 5, 2.5];
       const result = getPlateCombinations(input);
+      console.log(result);
+      // missing: [5, 25]
       expect(result.length).toEqual(31);
+      // expect(result).toEqual([
+      //   [45],
+      //   [25],
+      //   [10],
+      //   [5],
+      //   [2.5],
+      //   [2.5, 5],
+      //   [2.5, 10],
+      //   [5, 10],
+      //   [2.5, 25],
+      //   [5, 25],
+      //   [10, 25],
+      //   [2.5, 45],
+      //   [5, 45],
+      //   [10, 45],
+      //   [25, 45],
+      //   [2.5, 5, 10],
+      //   [2.5, 5, 25],
+      //   [2.5, 10, 25],
+      //   [5, 10, 25],
+      //   [2.5, 5, 45],
+      //   [2.5, 10, 45],
+      //   [5, 10, 45],
+      //   [2.5, 25, 45],
+      //   [5, 25, 45],
+      //   [10, 25, 45],
+      //   [2.5, 5, 10, 25],
+      //   [2.5, 5, 10, 45],
+      //   [2.5, 5, 25, 45],
+      //   [2.5, 10, 25, 45],
+      //   [5, 10, 25, 45],
+      //   [2.5, 5, 10, 25, 45],
+      // ]);
     });
 
     it("should return correct number of results for 6 plates", () => {
       const input = [55, 45, 25, 10, 5, 2.5];
       const result = getPlateCombinations(input);
+      console.log(result);
       expect(result.length).toEqual(63);
     });
 
@@ -89,6 +143,12 @@ describe("getPlateCombinations", () => {
       const input = [55, 45, 25, 10, 5, 2.5, 1.25];
       const result = getPlateCombinations(input);
       expect(result.length).toEqual(127);
+    });
+
+    it("should return correct number of results for 8 plates", () => {
+      const input = [55, 45, 35, 25, 10, 5, 2.5, 1.25];
+      const result = getPlateCombinations(input);
+      expect(result.length).toEqual(255);
     });
   });
 });
--- src/utils/getPlateCombinations.ts
+++ src/utils/getPlateCombinations.ts
@@ -1,6 +1,7 @@
 /**
  * Function for retrieving all possible combinations of plates.
- * This function has extremely poor performance and needs to be
+ *
+ * TODO: This function has extremely poor performance and needs to be
  * re-written, probably without recursion.
  *
  * @param plates
@@ -12,36 +13,61 @@ export default function getPlateCombinations(
     return [];
   }
 
-  const data: number[][] = [];
-  const plateCombinations = (arr: number[]): any => {
-    arr.map((plate, index, next) => {
-      const nextArray = [...next];
-      nextArray.splice(index, 1);
+  const resultSet = new Set();
 
-      if (!next.length || !nextArray.length) {
-        return;
-      }
+  if (plates.length === 1) {
+    return [[plates[0]]];
+  }
 
-      // add individual plates
-      if (!data.find((result) => result.toString() === [plate].toString())) {
-        data.push([plate]);
-      }
-
-      // add combinations
-      const combo = [plate, ...nextArray].sort((a, b) => a - b);
-      if (!data.find((result) => result.toString() === combo.toString())) {
-        data.push(combo);
-      }
-
-      // eslint-disable-next-line consistent-return
-      return plateCombinations(nextArray);
-    });
+  // add only unique values to the set
+  const addToSet = (newSet: number[]): void => {
+    newSet.sort((a, b) => a - b);
+    if (
+      !Array.from(resultSet).some(
+        (result) => (result as []).toString() === newSet.toString()
+      )
+    ) {
+      resultSet.add(newSet);
+    }
   };
 
-  if (plates.length > 1) {
-    plateCombinations(plates);
-  } else {
-    data.push([plates[0]]);
-  }
-  return data;
+  // add all provided plates
+  addToSet(plates);
+
+  plates
+    .sort((a, b) => a - b)
+    .forEach((plate: number, i: number, arr: number[]) => {
+      const otherPlates = [...arr];
+      otherPlates.splice(i, 1);
+
+      console.log("plate", plate);
+
+      // add individual plate
+      addToSet([plate]);
+      // add remaining plates
+      addToSet(otherPlates);
+
+      // get subsets
+      if (otherPlates.length > 1) {
+        otherPlates.forEach((otherPlate: number, j: number) => {
+          const subset = [...otherPlates];
+          const leftovers = subset.splice(0, 1 + j);
+
+          if (leftovers.length !== otherPlates.length) {
+            console.log("leftovers", leftovers);
+            addToSet(leftovers);
+            addToSet([plate, ...leftovers]);
+          }
+          if (subset.length) {
+            console.log("subset", subset);
+            addToSet(subset);
+            addToSet([plate, ...subset]);
+          }
+        });
+      }
+    });
+
+  const setArray = Array.from(resultSet);
+  setArray.sort((a, b) => (a as []).length - (b as []).length);
+  return setArray as number[][];
 }
