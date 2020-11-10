2d8b835b45cf65521559e9945c94b3011f73fcb5
Charlie Stanard <louis.stanard@gmail.com>
2020-11-10 07:29:30 -0700

    Added basic toggle styles

--- src/components/Results/UnitToggle.tsx
+++ src/components/Results/UnitToggle.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from "react";
+import cn from "classnames";
 
 import { LB, KG } from "../../constants/units";
 import {
@@ -9,14 +10,26 @@ import {
 import styles from "./UnitToggle.module.scss";
 
 const UnitToggle = (): ReactElement => {
-  const { setUnits }: GlobalUnitsContext = useGlobalUnitsContext();
+  const { units, setUnits }: GlobalUnitsContext = useGlobalUnitsContext();
 
   return (
     <div className={styles.container}>
-      <button type="button" onClick={(): void => setUnits(LB)}>
+      <button
+        type="button"
+        onClick={(): void => setUnits(LB)}
+        className={cn({
+          [styles.selected]: units === LB,
+        })}
+      >
         Pounds
       </button>
-      <button type="button" onClick={(): void => setUnits(KG)}>
+      <button
+        type="button"
+        onClick={(): void => setUnits(KG)}
+        className={cn({
+          [styles.selected]: units === KG,
+        })}
+      >
         Kilograms
       </button>
     </div>
