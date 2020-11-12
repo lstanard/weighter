9a623bcc465d8ea341104f7db17b331ec050d5cd
Charlie Stanard <charlie.stanard@fwi.com>
2020-11-12 06:22:48 -0700

    WIP on adding plates

--- src/components/EquipmentPanel/Plates.tsx
+++ src/components/EquipmentPanel/Plates.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { ReactElement, useCallback } from "react";
+import React, { ReactElement, useCallback, useState } from "react";
 import Select, { ValueType, StylesConfig } from "react-select";
+import { v4 as uuidv4 } from "uuid";
 
 import { Plate } from "../../types";
 import { EquipmentPanelProps } from "./EquipmentPanel";
@@ -9,6 +10,7 @@ import Button from "../Button";
 import DisplayWeight from "../DisplayWeight";
 
 import styles from "./Plates.module.scss";
+import { KG, LB } from "../../constants/units";
 
 // NOTE: should make this dynamically generated
 const PLATE_QTY_OPTIONS = [
@@ -20,6 +22,11 @@ const PLATE_QTY_OPTIONS = [
   { value: 12, label: "12" },
   { value: 14, label: "14" },
   { value: 16, label: "16" },
+];
+
+const PLATE_UNITS_OPTIONS = [
+  { value: LB, label: LB },
+  { value: KG, label: KG },
 ];
 
 type OptionType = { label: string; value: number };
@@ -65,6 +72,21 @@ export interface PlatesProps
   extends Pick<EquipmentPanelProps, "plates" | "updatePlates"> {}
 
 const Plates = ({ plates, updatePlates }: PlatesProps): ReactElement => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleCustomizePlates = useCallback(() => {
+    // Open a dialog? Not sure what I want to do here.
+    // Or it could take this current component and add some options?
+    // Might actually want to mock something up in Adobe XD first.
+
+    setIsEditing(!isEditing);
+  }, [isEditing]);
+
+  /**
+   * Toggle plate selection
+   *
+   * @param plate
+   */
   const handlePlateSelection = (plate: Plate): void => {
     updatePlates(
       plates.map((p: Plate) => {
@@ -79,6 +101,12 @@ const Plates = ({ plates, updatePlates }: PlatesProps): ReactElement => {
     );
   };
 
+  /**
+   * Update plate quantity
+   *
+   * @param value
+   * @param plate
+   */
   const handlePlateQtyChange = (value: number, plate: Plate): void => {
     updatePlates(
       plates.map((p: Plate) => {
@@ -93,6 +121,62 @@ const Plates = ({ plates, updatePlates }: PlatesProps): ReactElement => {
     );
   };
 
+  /**
+   * Remove plate
+   *
+   * @param plate
+   */
+  const handleRemovePlate = (plate: Plate): void => {
+    updatePlates(
+      plates.filter((p: Plate) => {
+        return p.id !== plate.id;
+      })
+    );
+  };
+
+  /**
+   * Add a new set of plates
+   */
+  const handleAddPlate = (): void => {
+    const addPlateWeight = document.getElementById(
+      "add-plate-weight"
+    ) as HTMLInputElement;
+    const addPlateQty = document.getElementById(
+      "add-plate-qty"
+    ) as HTMLInputElement;
+
+    const newPlate: Plate = {
+      id: uuidv4(),
+      weight: Number(addPlateWeight?.value),
+      weightUnits: LB,
+      quantity: Number(addPlateQty?.value),
+      selected: true,
+    };
+    updatePlates([...plates, newPlate]);
+
+    addPlateWeight.value = "";
+    addPlateQty.value = "";
+  };
+
+  /**
+   * This should really be its own component.
+   */
+  const addPlates = (
+    <div>
+      <label>Weight</label>
+      <input type="text" id="add-plate-weight" />
+      {/* Select for LB or KG */}
+      <Select options={PLATE_UNITS_OPTIONS} />
+      <label>Quantity</label>
+      <input type="text" id="add-plate-qty" />
+      <Button
+        id="add-plates-btn"
+        label="Add"
+        onClick={(): void => handleAddPlate()}
+      />
+    </div>
+  );
+
   const getDefaultSelectValue = useCallback(
     (plateQty: number, selected: boolean) => {
       const defaultValue = PLATE_QTY_OPTIONS.filter((option) => {
@@ -106,46 +190,59 @@ const Plates = ({ plates, updatePlates }: PlatesProps): ReactElement => {
     []
   );
 
-  const plateOptions = plates.map((plate) => {
-    const plateKey = `plate-${plate.id}`;
-    return (
-      <div key={plateKey} className={styles.plateRow}>
-        <Checkbox
-          id={plateKey}
-          defaultChecked={plate.selected}
-          onChange={(): void => handlePlateSelection(plate)}
-          className={styles.plateSelect}
-        />
-        <label
-          id={`label-${plateKey}`}
-          htmlFor={plateKey}
-          className={styles.plateLabel}
-        >
-          <DisplayWeight
-            weight={plate.weight}
-            weightClassName={styles.plateLabelWeight}
-            unitsClassName={styles.plateLabelUnits}
+  const plateOptions = plates
+    .sort((a, b) => a.weight - b.weight)
+    .map((plate) => {
+      const plateKey = `plate-${plate.id}`;
+      return (
+        <div key={plateKey} className={styles.plateRow}>
+          <Checkbox
+            id={plateKey}
+            defaultChecked={plate.selected}
+            onChange={(): void => handlePlateSelection(plate)}
+            className={styles.plateSelect}
           />
-        </label>
-        <span className={styles.plateQty}>
-          <span className={styles.plateQtyTimes}>x</span>
-          <Select
-            id={`${plateKey}-qty`}
-            placeholder="Qty."
-            onChange={(selectedOption: ValueType<OptionType>): void => {
-              const { value } = selectedOption as OptionType;
-              handlePlateQtyChange(value, plate);
-            }}
-            options={PLATE_QTY_OPTIONS}
-            className={styles.plateQtySelect}
-            styles={customSelectStyles}
-            defaultValue={getDefaultSelectValue(plate.quantity, plate.selected)}
-            aria-labelledby={`label-${plateKey}`}
-          />
-        </span>
-      </div>
-    );
-  });
+          <label
+            id={`label-${plateKey}`}
+            htmlFor={plateKey}
+            className={styles.plateLabel}
+          >
+            <DisplayWeight
+              weight={plate.weight}
+              weightClassName={styles.plateLabelWeight}
+              unitsClassName={styles.plateLabelUnits}
+            />
+          </label>
+          <span className={styles.plateQty}>
+            <span className={styles.plateQtyTimes}>x</span>
+            <Select
+              id={`${plateKey}-qty`}
+              placeholder="Qty."
+              onChange={(selectedOption: ValueType<OptionType>): void => {
+                const { value } = selectedOption as OptionType;
+                handlePlateQtyChange(value, plate);
+              }}
+              options={PLATE_QTY_OPTIONS}
+              className={styles.plateQtySelect}
+              styles={customSelectStyles}
+              defaultValue={getDefaultSelectValue(
+                plate.quantity,
+                plate.selected
+              )}
+              aria-labelledby={`label-${plateKey}`}
+            />
+          </span>
+          {isEditing && (
+            <button
+              type="button"
+              onClick={(): void => handleRemovePlate(plate)}
+            >
+              x
+            </button>
+          )}
+        </div>
+      );
+    });
 
   return (
     <div className={styles.container}>
@@ -153,10 +250,11 @@ const Plates = ({ plates, updatePlates }: PlatesProps): ReactElement => {
         <h2 className={styles.sectionTitle}>Weight Plates</h2>
       </header>
       <div className={styles.plateOptions}>{plateOptions}</div>
+      {isEditing && addPlates}
       <Button
-        id="add-plates-btn"
-        label="Add"
-        onClick={(): void => console.log("add plates")}
+        id="customize-plates-btn"
+        label={!isEditing ? "Customize" : "Done"}
+        onClick={handleCustomizePlates}
       />
     </div>
   );
